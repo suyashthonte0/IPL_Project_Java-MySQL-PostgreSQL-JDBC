ebd2b5615c56a60d77f79e676e671550cfceec36
suyashthonte <suyash.thonte@mountblue.tech>
2020-11-19 13:21:35 +0530

    Second Commit = Updated the statement for 5th case

--- src/controller/Run.java
+++ src/controller/Run.java
@@ -17,7 +17,7 @@ public class Run {
             System.out.println("2) Number of matches won of all teams over all the years of IPL.");
             System.out.println("3) For the year 2016 get the extra runs conceded per team.");
             System.out.println("4) For the year 2015 get the top economical bowlers.");
-            System.out.println("5) Create your own scenario.");
+            System.out.println("5) Create your own scenario :->> Number of wickets taken by each Bowler for all the years.");
             System.out.println("6) Exit.\n");
            // Scanner sc = new Scanner(System.in);
             int ch = sc.nextInt();
